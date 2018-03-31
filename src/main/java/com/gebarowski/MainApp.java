9c386714b6c801323f7fa2d3a838e5da427147ef
Michal Gebarowski <vmaaik@gmail.com>
2018-03-31 14:16:55 +0200

    initial commit

---
+++ src/main/java/com/gebarowski/MainApp.java
@@ -0,0 +1,10 @@
+package com.gebarowski;
+
+/**
+ * Created by Michał Gębarowski on 31/03/2018
+ */
+public class MainApp {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/gebarowski/tdd_inventory/InventoryItemImpl.java
@@ -0,0 +1,7 @@
+package com.gebarowski.tdd_inventory;
+
+/**
+ * Created by Michał Gębarowski on 31/03/2018
+ */
+public class InventoryItemImpl {
+}
